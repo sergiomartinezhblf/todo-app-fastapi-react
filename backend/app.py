d5b4a6962da498203ece88cbe2ad866290ae4b1f
Sergio Martinez <sergiomartinezhblf@htomail.com>
2026-04-28 21:28:58 -0600

    allow origins deploy in vercel

--- backend/app.py
+++ backend/app.py
@@ -10,11 +10,12 @@ app = FastAPI()
 
 origins = [
     "http//localhost:5173",
+    "https://todo-app-fastapi-react-xi.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
